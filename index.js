72a3b342de36d587ec86f5011a298e9bf21cc96b
Dan Kaplun <dbkaplun@gmail.com>
2015-05-26 07:10:59 -0400

    Tabs to spaces

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ var path = require('path');
 var fs = Promise.promisifyAll(require('fs'));
 
 module.exports = {
-	paths: {
-		hbs: path.join(__dirname, 'resume.hbs'),
-		less: path.join(__dirname, 'style.less')
-	},
-	render: Promise.method(function (resume) {
-		var paths = module.exports.paths;
-		return Promise.join(
-			// compile template
-			fs.readFileAsync(paths.hbs, 'utf8')
-				.then(Handlebars.compile),
+  paths: {
+    hbs: path.join(__dirname, 'resume.hbs'),
+    less: path.join(__dirname, 'style.less')
+  },
+  render: Promise.method(function (resume) {
+    var paths = module.exports.paths;
+    return Promise.join(
+      // compile template
+      fs.readFileAsync(paths.hbs, 'utf8')
+        .then(Handlebars.compile),
 
-			// compile styles
-			fs.readFileAsync(paths.less, 'utf8')
-				.then(function (style) { return less.render(style, {filename: paths.less, compress: true}); })
-				.then(function (res) { return res.css; }),
+      // compile styles
+      fs.readFileAsync(paths.less, 'utf8')
+        .then(function (style) { return less.render(style, {filename: paths.less, compress: true}); })
+        .then(function (res) { return res.css; }),
 
-			// compile styles and resume into template to create HTML
-			function (render, css) {
-				return render({resume: resume, css: css});
-			});
-	})
+      // compile styles and resume into template to create HTML
+      function (render, css) {
+        return render({resume: resume, css: css});
+      });
+  })
 };
