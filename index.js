7cf944b59880c230cdcb3bb49cf83e3e9493d52d
Dan Kaplun <dbkaplun@gmail.com>
2015-05-26 07:09:06 -0400

    index.js asynchronous (depends on jsonresume/resume-cli#118)

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-var fs = require("fs");
-var Handlebars = require("handlebars");
-
-function render(resume) {
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
-	return Handlebars.compile(tpl)({
-		css: css,
-		resume: resume
-	});
-}
+var Handlebars = require('handlebars');
+var less = require('less');
+var Promise = require('bluebird');
+var path = require('path');
+var fs = Promise.promisifyAll(require('fs'));
 
 module.exports = {
-	render: render
+	paths: {
+		hbs: path.join(__dirname, 'resume.hbs'),
+		less: path.join(__dirname, 'style.less')
+	},
+	render: Promise.method(function (resume) {
+		var paths = module.exports.paths;
+		return Promise.join(
+			// compile template
+			fs.readFileAsync(paths.hbs, 'utf8')
+				.then(Handlebars.compile),
+
+			// compile styles
+			fs.readFileAsync(paths.less, 'utf8')
+				.then(function (style) { return less.render(style, {filename: paths.less, compress: true}); })
+				.then(function (res) { return res.css; }),
+
+			// compile styles and resume into template to create HTML
+			function (render, css) {
+				return render({resume: resume, css: css});
+			});
+	})
 };
