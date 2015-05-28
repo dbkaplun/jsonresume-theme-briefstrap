2e05571b47de684086d80054c0b571e84db6d41b
Dan Kaplun <dbkaplun@gmail.com>
2015-05-28 03:17:23 -0400

    Adds JS pipeline, fixes social buttons

---
+++ compile.js
@@ -0,0 +1,48 @@
+var Handlebars = require('handlebars');
+var less = require('less');
+var Promise = require('bluebird');
+var path = require('path');
+var fs = Promise.promisifyAll(require('fs'));
+var browserify = require('browserify');
+
+Handlebars.registerHelper('randomColor', function () {
+	var channels = [];
+	for (var i = 0; i < 3; i++) channels.push(Math.random()*192|0);
+  return 'rgb('+channels.join(',')+')';
+});
+
+Handlebars.registerHelper('lowercase', function (str) {
+	return str.toLowerCase();
+});
+
+module.exports = {
+  paths: {
+    hbs: path.join(__dirname, 'resume.hbs'),
+    less: path.join(__dirname, 'index.less'),
+		js: path.join(__dirname, 'index.js')
+  },
+  render: Promise.method(function (resume) {
+    var paths = module.exports.paths;
+    return Promise.join(
+      // compile template
+      fs.readFileAsync(paths.hbs, 'utf8')
+        .then(Handlebars.compile),
+
+      // compile styles
+      fs.readFileAsync(paths.less, 'utf8')
+        .then(function (style) { return less.render(style, {filename: paths.less, compress: true}); })
+        .then(function (res) { return res.css; }),
+
+			(function () {
+				var b = Promise.promisifyAll(browserify({
+					entries: paths.js
+				}));
+				return b.bundleAsync();
+			})(),
+
+      // compile styles and resume into template to create HTML
+      function (render, css, js) {
+        return render({resume: resume, css: css, js: js});
+      });
+  })
+};
--- index.js
+++ index.js
@@ -1,39 +1,7 @@
-var Handlebars = require('handlebars');
-var less = require('less');
-var Promise = require('bluebird');
-var path = require('path');
-var fs = Promise.promisifyAll(require('fs'));
+var $ = window.jQuery = require('jquery');
+require('bootstrap/js/tooltip');
 
-Handlebars.registerHelper('labelColor', function () {
-	var channels = [];
-	for (var i = 0; i < 3; i++) channels.push(Math.random()*192|0);
-  return 'rgb('+channels.join(',')+')';
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip({container: 'body'});
+  $('#print-button').click(function () { window.print(); });
 });
-
-Handlebars.registerHelper('lowercase', function (str) {
-	return str.toLowerCase();
-});
-
-module.exports = {
-  paths: {
-    hbs: path.join(__dirname, 'resume.hbs'),
-    less: path.join(__dirname, 'style.less')
-  },
-  render: Promise.method(function (resume) {
-    var paths = module.exports.paths;
-    return Promise.join(
-      // compile template
-      fs.readFileAsync(paths.hbs, 'utf8')
-        .then(Handlebars.compile),
-
-      // compile styles
-      fs.readFileAsync(paths.less, 'utf8')
-        .then(function (style) { return less.render(style, {filename: paths.less, compress: true}); })
-        .then(function (res) { return res.css; }),
-
-      // compile styles and resume into template to create HTML
-      function (render, css) {
-        return render({resume: resume, css: css});
-      });
-  })
-};
