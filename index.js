e49190bdf1ce622e8724eed27bbb7e8066e424f1
Dan Kaplun <dbkaplun@gmail.com>
2015-05-30 13:10:39 -0400

    Removes unused bootstrap/tooltip from JS

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var $ = window.jQuery = require('jquery');
-require('bootstrap/js/tooltip');
 
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip({container: 'body'});
-  $('#print-button').click(function (evt) { evt.preventDefault(); window.print(); });
+  $('#print-button').click(function (evt) {
+    evt.preventDefault();
+    window.print();
+  });
 });
